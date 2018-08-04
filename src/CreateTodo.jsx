b1b22ec4cc64a17c36a22b0c973accfc3aa1378f
babramczyk <bjabramczyk@gmail.com>
2018-08-04 12:10:10 -0500

    feat(CreateTodo): send up actual user text

--- src/CreateTodo.jsx
+++ src/CreateTodo.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
 
 class CreateTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
   render() {
     return (
       <form className="create-todo" onSubmit={e => this.handleSubmit(e)}>
-        <input type="text" />
+        <input
+          type="text"
+          value={this.state.text}
+          onChange={e => this.handleTextChange(e)}
+        />
         <input type="submit" value="Add" />
       </form>
     );
   }
 
+  handleTextChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
   handleSubmit(e) {
     e.preventDefault();
-    this.props.onAdd("hey");
+    this.props.onAdd(this.state.text);
   }
 }
 
